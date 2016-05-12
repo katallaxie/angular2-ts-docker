71350ac690580c14915394c9a7799ea08a2c53a1
Sebastian Döll <sebastian@pixelmilk.com>
2016-05-12 17:28:42 +0200

    ~ updating env script

--- payload/run.sh
+++ payload/run.sh
@@ -2,15 +2,23 @@
 
 echo "Hello, to the development ..."
 
+DIR="/app"
+
 # changing username on
 usermod -u $USER_ID nodejs
 
 # link modules
-if [ ! -d "/app/node_modules" ]; then
+if [ ! -d "${DIR}/node_modules" ]; then
   ln -s /tmp/node_modules /app
 fi
 
-# run the grunt
-typings install
+# typings
+if [ ! -d "${DIR}/typings" ]; then
+  typings install
+fi
+
+echo "Setting up environment ..."
 grunt init
+
+echo "Running environment ..."
 grunt fun
